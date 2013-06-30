a1ac9a453c26ba9fde597f956323e1036df68801
Knut Saua Mathiesen <knut@canutes.net>
2013-06-30 21:17:30 +0200

    Intial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to lastfm-visualizer
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/lastfm_visualizer/core_test.clj
@@ -0,0 +1,7 @@
+(ns lastfm-visualizer.core-test
+  (:require [clojure.test :refer :all]
+            [lastfm-visualizer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
